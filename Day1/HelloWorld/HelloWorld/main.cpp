8acc7216454bf34826243dbbb06d7b68be5cbfbc
jbarto15 <joshuabarton222@gmail.com>
2023-08-21 12:20:06 -0600

    Committing my HelloWorld project to Git.

---
+++ Day1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Josh Barton on 8/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+        // insert code here...
+        std::cout << "Hello, World!\n";
+        std::cout << "My name is Josh.\n";
+        std::cout << "2 + 2 = " << "4" <<"\n";
+        return 0;
+    }
