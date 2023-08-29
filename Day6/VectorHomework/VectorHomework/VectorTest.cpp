bedc1f5e6c8280c04d2b75b8c3473c8355e18fb8
jbarto15 <joshuabarton222@gmail.com>
2023-08-28 18:09:56 -0600

    This is an updated version of my Vector homework project. The first one was not updating to gitHub correctly. This one should have all the correct files.

---
+++ Day6/VectorHomework/VectorHomework/VectorTest.cpp
@@ -0,0 +1,111 @@
+/*
+  Author: Daniel Kopta and ??
+  July 2017
+  
+  CS 6010 Fall 2019
+  Vector util library tests
+  Compile this file together with your VectorUtil library with the following command:
+  clang++ -std=c++11 VectorTest.cpp VectorUtil.cpp
+  Most of the provided tests will fail until you have provided correct 
+  implementations for the VectorUtil library functions.
+  You will need to provide more thorough tests.
+*/
+
+#include <iostream>
+#include <string>
+
+// Include the VectorUtil library
+#include "VectorUtil.h"
+
+/*
+ * Helper function for failing a test.
+ * Prints a message and exits the program.
+ */
+void ErrorExit( std::string message )
+{
+  std::cerr << "Failed test: " << message << std::endl;
+  exit(1); // Causes the entire program to exit.
+}
+
+
+int main()
+{
+  
+  // Set up some input vectors for testing purposes.
+
+  // We can use list initialization syntax:
+  vector<int> v1 = {3, 1, 0, -1, 5};
+
+  // Or we can repeatedly push_back items
+  vector<int> v2;
+  v2.push_back(1);
+  v2.push_back(2);
+  v2.push_back(3);
+
+  // When testing, be sure to check boundary conditions, such as an empty vector
+  vector<int> empty;
+  
+  
+  /* 
+   * Contains tests 
+   */
+
+  // v1 doesn't contain 4, so this should return false
+  if( Contains(v1, 4) ) {
+    ErrorExit( "Contains() - test 1" );
+  }
+
+  // v1 does contain -1, so this should return true
+  if(!Contains(v1, -1)) {
+    ErrorExit("Contains() - test 2");
+  }
+
+  /* 
+   * The vector 'empty' doesn't contain anything, so this should return false
+   * The specific value we're looking for here (99) is not important in this test. 
+   * This test is designed to find any general errors caused by the array being empty. 
+   * That type of error is unlikely to depend on the value we are looking for.
+  */
+  if( Contains(empty, 99) ) {
+    ErrorExit("Contains() - empty");
+  }
+  
+  // TODO: Add your own tests that thoroughly exercise your VectorUtil library.
+  
+  //edge case test for the FindMin function
+    if (FindMin(v1) != -1) {
+        ErrorExit("FindMin() - not sorting correctly");
+    }
+  //testing if minimum of v2 is correct
+    if (FindMin(v1) != -1) {
+        ErrorExit("FindMin() is incorrect");
+    }
+  //edge case test for the FindMax function
+    if (FindMax(v1) == 3) {
+        ErrorExit("FindMax() - not sorting correctly");
+    }
+  
+  //testing if maximum of v1 is correct
+    if (FindMax(v1) != 5) {
+        ErrorExit("FindMax() is incorrect");
+    }
+    
+  //tests for the average function
+    if (Average(v2) != 2) {
+        ErrorExit("Average() - not computing average correctly");
+    }
+    
+  //Error message that prints whether or not v1 is sorted or not. If it is not sorted it will display the error message. 
+    if (!IsSorted(v2)) {
+        ErrorExit("isSorted() - false");
+    }
+
+  // Since any failed test exits the program, if we made it this far, we passed all tests.
+  std::cout << "All tests passed!\n";
+
+  
+    Average(v1);
+    std::cout << IsSorted(v2) << std::endl;
+    std::cout << FindMin(v1) << std::endl;
+    std::cout << FindMax(v1) << std::endl;
+}
