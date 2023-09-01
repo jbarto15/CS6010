fc73b880b1c83aee58b89340ddea75dc0c0d1e3e
jbarto15 <joshuabarton222@gmail.com>
2023-09-01 17:11:23 -0600

    Book Analyzer assignment

--- Day9/BookAnalyzer/BookAnalyzer/main.cpp
+++ Day9/BookAnalyzer/BookAnalyzer/main.cpp
@@ -8,28 +8,39 @@
 #include <iostream>
 #include <fstream> //file stream
 #include <cstdlib> // For exit() function
+#include <vector>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     
     //create stream variable that will input the book
-    std::ifstream bookStream("frakenstein.txt");
-    
-    //create a string varible that the stream will go into
-    std::string bookText;
-    
-    //stream in the book to the variable
-    bookStream >> bookText;
-    
+    std::ifstream inputBookStream;//("frankenstein.txt");
+    inputBookStream.open("/Users/joshbarton/Downloads/frankenstein.txt");
+
     //check to see if book stream opened properly
-//    std::string bookText = (“texasFootball.txt”);
-//    std::ifstream bookStream( bookText );
-//
-//    if( bookStream.fail() ) {
-//        std::cout << “Failed to open file: “ << bookText << “.\n”;
-//        exit( 1 ); // Kill the program and exit with a return value of 1
-//
-//    }
+    if( inputBookStream.fail() ) {
+        std::cout << "Failed to open file.\n";
+        // Kill the program and exit with a return value of 1
+        exit(1);
+
+    }
+    
+    //create a string varible that the file text will go into
+    std::string word;
+
+    //stream in the book to the variable
+    inputBookStream >> word;
+    
+    //create a vector to store the words from the book
+    std::vector<std::string> allWordsOfBook;
+
+    //use a while loop to stream in the entire book
+    while (inputBookStream >> word) {
+        std::cout << "Read in: " << word << "\n";
+        allWordsOfBook.push_back(word);
+    }
+
+    
     
     return 0;
 }
